708038cf74ad044d51b50b88d28c5d16d6193361
Ladon <wb@wolfgang-braunstingl.at>
2025-10-28 13:43:12 +0100

    Änderung an der Oberfläche und einfügen eines Log-Lösch-Btn und Remote Cfg

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from MainWindow import Ui_MainWindow
 from PyQt6.QtSerialPort import QSerialPort, QSerialPortInfo
 from PyQt6.QtCore import QIODevice, pyqtSlot
 
+from PyQt6.QtGui import QRegularExpressionValidator
+from PyQt6.QtCore import QRegularExpression
+
+#Globale Variablen
+serialNumber = 0
+
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj=None, **kwargs):
         super().__init__(*args, **kwargs)
@@ -24,7 +30,21 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.cb_parityBit.setCurrentIndex(0)
 
         self.btn_readPorts.clicked.connect(self.list_available_ports)
+        self.btn_clearLog.clicked.connect(self.clear_log)
 
+        # Regulärer Ausdruck für nur Ziffern
+        regex = QRegularExpression("^\\d+$")
+        validator = QRegularExpressionValidator(regex)
+
+        self.tb_serialNumberRemote.setValidator(validator)
+        self.tb_serialNumberRemote.setMaxLength(7)
+
+        self.btn_setSerialNumber.clicked.connect(self.set_serial_number)
+        self.btn_open.clicked.connect(self.send_open_cmd)
+
+    # Funktion zum Löschen des Log-Fensters
+    def clear_log(self):
+        self.tb_logOutput.clear()
 
     # Funktion zum Finden und Anzeigen verfügbarer Ports
     def list_available_ports(self):
@@ -35,7 +55,12 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.tb_logOutput.append(f"Name: {port.portName()}, Beschreibung: {port.description()}")    
             self.cb_serialPorts.addItem(port.portName())
 
-
+    def set_serial_number(self):
+        global serialNumber
+        serialNumber = self.tb_serialNumberRemote.text()
+        
+    def send_open_cmd(self):
+        print("Serialnumner: ", serialNumber)
 
 app = QtWidgets.QApplication(sys.argv)
 
